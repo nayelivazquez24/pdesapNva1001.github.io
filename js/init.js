2dd89e415598d570fc8d713fd6938857561c05fb
nayelivazquez24 <93957569+nayelivazquez24@users.noreply.github.com>
2021-11-23 19:44:21 -0600

    Update init.js

--- js/init.js
+++ js/init.js
@@ -8,11 +8,11 @@
  * servidor. */
 // @ts-ignore
 firebase.initializeApp({
- apiKey: "AIzaSyDgSfpnDIeA8aIjm0wU9hJACw-rMeXChFk",
-  authDomain: "bdusuarios-8335f.firebaseapp.com",
-  projectId: "bdusuarios-8335f",
-  storageBucket: "bdusuarios-8335f.appspot.com",
-  messagingSenderId: "542903782789",
-  appId: "1:542903782789:web:b828cc664f2542ce5b48ec",
-  measurementId: "G-531PHY00BC"
+ apiKey: "AIzaSyDFImbZKoj0kLCPve-Bc1-OiwKzHQI1kvY",
+  authDomain: "bdusuarios-42525.firebaseapp.com",
+  projectId: "bdusuarios-42525",
+  storageBucket: "bdusuarios-42525.appspot.com",
+  messagingSenderId: "273188808778",
+  appId: "1:273188808778:web:ebd32a6efa37acfaf49eb3",
+  measurementId: "G-B0ZDRJX7CS"
 });
